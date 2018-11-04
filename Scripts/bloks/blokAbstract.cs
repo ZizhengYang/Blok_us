5cf6ab190a83f0863a337c2af135ac72bcadc0b9
Zizheng Yang <32321397+procccp@users.noreply.github.com>
2018-11-04 12:33:35 -0600

    Add files via upload

---
+++ Scripts/bloks/blokAbstract.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class blokAbstract : MonoBehaviour {
+
+    protected GameObject obj0 = null;
+    protected GameObject obj1 = null;
+    protected GameObject obj2 = null;
+    protected GameObject obj3 = null;
+    protected GameObject obj4 = null;
+
+    protected GameObject[] objArray = new GameObject[5];
+
+    void Start()
+    {
+        objArray[0] = obj0;
+        objArray[1] = obj1;
+        objArray[2] = obj2;
+        objArray[3] = obj3;
+        objArray[4] = obj4;
+    }
+
+}
