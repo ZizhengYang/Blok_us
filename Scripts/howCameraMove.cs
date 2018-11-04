1876b785121c8be8079424a1b87f6fdaf7bf3bef
Zizheng Yang <32321397+procccp@users.noreply.github.com>
2018-11-04 11:56:31 -0600

    Add files via upload

--- Scripts/howCameraMove.cs
+++ Scripts/howCameraMove.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class howCameraMove : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    private const int speed = 72;
+    private const int palstance = 48;
+
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
@@ -13,4 +16,46 @@ public class howCameraMove : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    void FixedUpdate()
+    {
+        // the free moveing part
+        //-X
+        if (Input.GetKey(KeyCode.X))
+        {
+            transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+        }
+        else if (Input.GetKey(KeyCode.Z))
+        {
+            transform.Translate(new Vector3(0, 0, -1 * speed * Time.deltaTime));
+        }
+        //-Y
+        else if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-1 * speed * Time.deltaTime, 0, 0));
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        //-Z
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(new Vector3(0, -1 * speed * Time.deltaTime, 0));
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+
+        // the rotation part
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(new Vector3(0, 0, palstance * Time.deltaTime));
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(new Vector3(0, 0, -1 * palstance * Time.deltaTime));
+        }
+    }
 }
